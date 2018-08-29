a60dff521c200d7eb499d347a08155f84578ad41
Nicol <nickkat95@gmail.com>
2018-08-29 18:43:22 +0300

    Adding commands

--- angular/src/app/main-view/system-view/site-view/server-view/command-list/command/command.component.ts
+++ angular/src/app/main-view/system-view/site-view/server-view/command-list/command/command.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Command} from "../../../../../../models/command.model";
 
 @Component({
   selector: 'app-command',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./command.component.css']
 })
 export class CommandComponent implements OnInit {
+  @Input() command: Command;
 
   constructor() { }
 
